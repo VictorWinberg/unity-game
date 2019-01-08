e0213e6d7bb3cab913897e3801345d5f4f9779aa
Victor Winberg <victor.m.winberg@gmail.com>
2019-01-08 17:01:55 +0100

    Refactor DistDot ItemCtrl collision

--- Assets/Scripts/Item/ItemController.cs
+++ Assets/Scripts/Item/ItemController.cs
@@ -7,19 +7,16 @@ public class ItemController : MonoBehaviour {
     private GameObject item;
 
     public void Move () {
+        float maxDist = 2f;
+        float minDotDist = Mathf.Infinity;
+
         if (this.item == null) {
             int pickableLayer = 1 << LayerMask.NameToLayer ("Pickable");
-            float maxDist = 2f;
-            float minDotDist = Mathf.Infinity;
             GameObject item = null;
             foreach (Collider hit in Physics.OverlapSphere (transform.position, maxDist, pickableLayer)) {
-                Vector2 hitPos = new Vector2 (hit.transform.position.x, hit.transform.position.z);
-                Vector2 myPos = new Vector2 (transform.position.x, transform.position.z);
-                Vector2 forward = new Vector2 (transform.forward.x, transform.forward.z);
-                float dist = Vector2.Distance (hitPos, myPos);
-                float dot = Vector2.Dot (forward, (hitPos - myPos).normalized);
-                if (dist < minDotDist && dot > 0.5f) {
-                    minDotDist = dist * dot;
+                Utility.DistDot dd = Utility.DistDotXZ (hit, transform);
+                if (dd.distDot < minDotDist && dd.dot > 0.5f) {
+                    minDotDist = dd.distDot;
                     item = hit.gameObject;
                 }
             }
@@ -27,17 +24,11 @@ public class ItemController : MonoBehaviour {
                 PickUp (item);
             }
         } else {
-            float maxDist = 2f;
-            float minDotDist = Mathf.Infinity;
             GameObject container = null;
             foreach (Collider hit in Physics.OverlapSphere (transform.position, maxDist)) {
-                Vector2 hitPos = new Vector2 (hit.transform.position.x, hit.transform.position.z);
-                Vector2 myPos = new Vector2 (transform.position.x, transform.position.z);
-                Vector2 forward = new Vector2 (transform.forward.x, transform.forward.z);
-                float dist = Vector2.Distance (hitPos, myPos);
-                float dot = Vector2.Dot (forward, (hitPos - myPos).normalized);
-                if (dist * dot < minDotDist && dot > 0.5f && hit.gameObject.tag == "Interactable") {
-                    minDotDist = dist * dot;
+                Utility.DistDot dd = Utility.DistDotXZ (hit, transform);
+                if (dd.distDot < minDotDist && dd.dot > 0.5f && hit.gameObject.tag == "Interactable") {
+                    minDotDist = dd.distDot;
                     container = hit.gameObject;
                 }
             }
@@ -54,10 +45,9 @@ public class ItemController : MonoBehaviour {
         float minDotDist = Mathf.Infinity;
         GameObject item = null;
         foreach (Collider hit in Physics.OverlapSphere (transform.position, maxDist)) {
-            float dist = Vector3.Distance (hit.transform.position, transform.position);
-            float dot = Vector3.Dot (transform.forward, (hit.transform.position - transform.position).normalized);
-            if (dist * dot < minDotDist && dot > 0.5f && hit.gameObject.tag == "Interactable") {
-                minDotDist = dist * dot;
+            Utility.DistDot dd = Utility.DistDotXZ (hit, transform);
+            if (dd.distDot < minDotDist && dd.dot > 0.5f && hit.gameObject.tag == "Interactable") {
+                minDotDist = dd.distDot;
                 item = hit.gameObject;
             }
         }
---
+++ Assets/Scripts/Utility/Utility.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class Utility {
+
+	/** Fisher-Yates shuffle method to randomize the positions */
+	public static T[] ShuffleArray<T> (T[] array, int seed) {
+		System.Random r = new System.Random (seed);
+
+		for (int i = 0; i < array.Length - 1; i++) {
+			int randomIndex = r.Next (i, array.Length);
+			T temp = array[randomIndex];
+			array[randomIndex] = array[i];
+			array[i] = temp;
+		}
+
+		return array;
+	}
+
+	public static DistDot DistDotXZ (Collider hit, Transform transform) {
+		Vector2 hitPos = new Vector2 (hit.transform.position.x, hit.transform.position.z);
+		Vector2 myPos = new Vector2 (transform.position.x, transform.position.z);
+		Vector2 forward = new Vector2 (transform.forward.x, transform.forward.z);
+		float dist = Vector2.Distance (hitPos, myPos);
+		float dot = Vector2.Dot (forward, (hitPos - myPos).normalized);
+		return new DistDot (dist, dot);
+	}
+
+	public class DistDot {
+		public float dist { get; private set; }
+		public float dot { get; private set; }
+		public float distDot { get; private set; }
+		public DistDot (float dist, float dot) {
+			this.dist = dist;
+			this.dot = dot;
+			this.distDot = dist / dot;
+		}
+	}
+}
